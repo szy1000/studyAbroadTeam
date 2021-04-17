f84c39fc2606954a2da6b9929e4b2aefef900dc5
szy1000 <1538078053@qq.com>
2021-04-17 23:55:41 +0800

    add react native element

--- App.js
+++ App.js
@@ -8,49 +8,11 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, useColorScheme, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Header, SearchBar} from 'react-native-elements';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -60,33 +22,33 @@ const App: () => Node = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <SafeAreaProvider style={backgroundStyle}>
+      <Header
+        placement="left"
+        leftComponent={{icon: 'menu', color: '#fff'}}
+        centerComponent={
+          <View
+            style={{width: '100%', height: 50, backgroundColor: 'blue'}}
+            // onChangeText={this.updateSearch}
+            value="sss">
+            <SearchBar
+              style={{
+                backgroundColor: 'transparent',
+              }}
+              placeholder="Type Here..."
+              // onChangeText={this.updateSearch}
+              value=""
+            />
+          </View>
+        }
+        rightComponent={{icon: 'home', color: '#fff'}}
+      />
+
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+      />
+    </SafeAreaProvider>
   );
 };
 
